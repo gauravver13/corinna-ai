27d5c5145ad5908c49f0af75b4a6f1a477ec6968
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2024-08-14 18:42:02 +0530

    add next/images and clerk auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+import { authMiddleware } from '@clerk/nextjs';
+export default authMiddleware({
+    publicRoutes: ["/", "/auth(.*)","/portal(.*)"],
+    ignoredRoutes: ['/chatbot'],
+})
 
 export const config = {
       matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }
-
-
-
-
-
-
-// // import { authMiddleware } from '@clerk/nextjs';
-// export default authMiddleware({
-//     publicRoutes: ["/", "/auth(.*)","/portal(.*)"],
-//     ignoredRoutes: ['/chatbot'],
-// })
-
-// export const config = {
-//       matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// }
